28417557fedef23d90544b8820c39fb8b8862120
unknown <zwodek01@gmail.com>
2019-05-12 11:04:02 +0200

    complete

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 let counter = 1;
 
+const list = document.getElementById('list');
 // Add Task
 function addTask() {
     const inputText = $('#input').val();
 
     // Alert if input is empty
     if (inputText.length === 0) {
-        const newTask = '<div class="div"><p class="to-do__alert">Write a task</p><button class="to-do__btn--ok to-do__btn to-do__add" id="btnOk">OK</button</div>';
+        const newTask = '<div class="div"><p class="to-do__alert">Write a task name</p><button class="to-do__btn--ok to-do__btn to-do__add" id="btnOk">OK</button</div>';
         $('#main').append(newTask);
 
         // Blur content
@@ -25,21 +26,22 @@ function addTask() {
     }
 
 
-
     // Count task, Add new task and description
     const numberItem = $('.to-do__listItem').length + 1;
-    const newLi = `<li class="to-do__listItem id="listItem"><a class="remove">x</a><p class="to-do__text-task-number">Task number ${counter}</p></li>`;
+    const newLi = `<li class="to-do__listItem" id="listItem"><a class="remove">x</a><p class="to-do__text-task-number">Task number ${counter}</p></li>`;
     $('#list').append(newLi);
     const description = '<p class="to-do__text-task-description">' + inputText + '</p> <input class="checkbox" type="checkbox">';
     $(".to-do__listItem:nth-of-type(" + numberItem + ")").append(description);
     counter++;
 
-
-
     // Clear text form input
     $('input').val('');
 
+    // localstorage 
+
+    localStorage["list"] = list.innerHTML;
 }
+
 
 // Remove alert input
 $(document).on('click', '#btnOk', function () {
@@ -52,21 +54,43 @@ function removeTask() {
     $('.to-do__listItem:last-child').remove();
     counter--;
 
+    // localstorage 
+
+    localStorage["list"] = list.innerHTML;
+
     //Reset counter
     if ($('.to-do__listItem').length == 0) {
         counter = 1;
     }
 }
 
+// localstorage 
+
+if (localStorage["list"]) {
+    list.innerHTML = localStorage["list"];
+}
+
 // Clear all tasks button
 function clearTask() {
     $('.to-do__listItem').remove();
     counter = 1;
+
+    // localstorage 
+
+    localStorage["list"] = list.innerHTML;
 }
 
 // Delete Task
 $(document).on('click', '.remove', function () {
     $(this).parent().remove();
+
+    // localstorage 
+    localStorage["list"] = list.innerHTML;
+
+    //Reset counter
+    if ($('.to-do__listItem').length == 0) {
+        counter = 1;
+    }
 });
 
 // Checkbox
@@ -79,6 +103,9 @@ $(document).on('change', '.checkbox', function () {
         $(this).attr('checked', 'checked');
     }
     $(this).parent().toggleClass('completed');
+
+    // localstorage 
+    localStorage["list"] = list.innerHTML;
 });
 
 // Click enter to add task
@@ -87,7 +114,6 @@ function enterClick() {
         $('#btnAdd').click();
     }
 }
-
 
 $(document).ready(function () {
     $('#btnAdd').on('click', addTask);
